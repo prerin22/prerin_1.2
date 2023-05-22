1635e98bb3ef0bcd4a8361a4b9b020e9968d4949
prerin22 <parthbhadani8467@gmail.com>
2023-05-22 12:39:47 +0530

    1.2

---
+++ PRERIN.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+clrscr();
+printf("--------\n");
+printf("|\t|\n");
+printf("R\t|\n");
+printf("N\t|\n");
+printf("W\t|\n");
+printf("|\t|\n");
+printf("--------");
+getch();
+
+
+}
---
+++ PRERIN3.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+clrscr();
+printf("*\n*\n*\n*       * *         *");
+printf("\n*      *    *      *\n");
+printf("*    *       *    *\n");
+printf("*  *           * *\n");
+printf("* *\n*");
+getch();
+}
